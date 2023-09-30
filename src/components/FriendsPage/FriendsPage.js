17a4b6ab3ca05cffacdff66c2eb977cf85690888
jihirmas <jihirmas@miuandes.cl>
2023-09-30 18:59:14 -0300

    cambios guti

--- src/components/FriendsPage/FriendsPage.js
+++ src/components/FriendsPage/FriendsPage.js
@@ -1,15 +1,126 @@
-//Create starter component
-import React from 'react';
-import { Box } from "@mui/material";
-
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Typography } from "@mui/material";
+import { QrReader } from 'react-qr-reader';
 import SpinnerOfDoom from "../HomePage/SpinnerOfDoom";
 
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
 function FriendsPage(props) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [userLocation, setUserLocation] = useState(null);
+    const [qrCodeSrc, setQrCodeSrc] = useState(null);
+    const [isScannerOpen, setIsScannerOpen] = useState(false); // State to control QR code scanner
+
+    // Function to get the user's location
+    const getUserLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+                setUserLocation({ lat: latitude, lng: longitude });
+            }, (error) => {
+                console.error("Error getting user location:", error);
+            });
+        } else {
+            console.error("Geolocation is not supported by this browser.");
+        }
+    };
+
+    // Function to handle the API fetch
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            // Perform your API fetch here
+            const response = await fetch('http://localhost:3000/api/v1/friendship_tokens/show', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    'user_id': localStorage.getItem('user_id'),
+                    'latitude': userLocation ? userLocation.lat : null,
+                    'longitude': userLocation ? userLocation.lng : null,
+                }),
+            });
+
+            // Check if the response is successful (status code 200)
+            if (response.status === 200) {
+                // Convert the response body to Blob and create a data URL
+                const blob = await response.blob();
+                const qrCodeDataUrl = URL.createObjectURL(blob);
+                setQrCodeSrc(qrCodeDataUrl); // Set the image source
+            } else {
+                console.error("API request failed with status:", response.status);
+            }
+        } catch (error) {
+            // Handle any errors that occurred during the fetch
+            console.error("Error fetching data:", error);
+        }
+
+        setIsLoading(false);
+    };
+
+    // Function to handle the scanning of the QR code
+    const handleScan = (data) => {
+        if (data) {
+            // Close the scanner and handle the scanned data (e.g., send a request via API fetch)
+            setIsScannerOpen(false);
+
+            // Here, you can use the 'data' variable, which contains the scanned QR code data.
+            console.log("Scanned QR code data:", data);
+
+            // Send a request via API fetch with the scanned data
+            // You can implement this logic here.
+            // Example: sendFriendRequest(data);
+        }
+    };
+
+    const handleError = (error) => {
+        console.error("QR code scanning error:", error);
+    };
+
+    useEffect(() => {
+        // Get the user's location when the component mounts
+        getUserLocation();
+    }, []);
+
     return (
-        <Box>
-            < SpinnerOfDoom color={'secondary.main'} />
-        </Box>
+        <div style={{ padding: '20px', height: '600px', width: '850px', margin: '0 auto', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Button style={{ marginTop: '90px' }} variant='contained' color="primary" size="small" onClick={fetchData}>
+                Crear Solicitud de Amistad
+            </Button>
+
+            <div style={{ marginTop: '20px' }}>
+                {qrCodeSrc && (
+                    <div>
+                        <Typography variant="h6">Tu código QR:</Typography>
+                        <img src={qrCodeSrc} alt="QR Code" style={{ width: '200px', height: '200px' }} />
+                    </div>
+                )}
+            </div>
+
+            {isScannerOpen && (
+                <div style={{ marginTop: '20px' }}>
+                    <QrReader
+                        delay={300}
+                        onError={handleError}
+                        onScan={handleScan}
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            )}
+
+            {!isScannerOpen && (
+                <Button style={{ marginTop: '20px' }} variant='contained' color="secondary" size="small" onClick={() => setIsScannerOpen(true)}>
+                    Escanear Amigo
+                </Button>
+            )}
+        </div>
     );
 }
 
 export default FriendsPage;
+
+
